9234553dd68d09f5631289da040f11d2c5299de1
guccijenkins <devinswydev@gmail.com>
2024-12-30 19:59:02 -0700

    12/30/24 Update to code that prints "X" and "O" for player 1 and 2 respectively.

--- main.py
+++ main.py
@@ -1,34 +1,64 @@
-board = [['','|','','|',''],
+# set up the tic tac toe board
+board = [['', '|', '', '|', ''],
             ['-------'],
-        ['','|','','|',''],
+        ['', '|', '', '|', ''],
             ['-------'],
-        ['','|','','|','']]
+        ['', '|', '', '|', '']]
 
+# set up the player inputs for row and column, with accompanied list and index number.
+# For example; [1,1],[0,0], where [1,1] is the player input for row and column number,
+# and [0,0] is the list number and the index number for that list.
 board_positions = [[[1,1],[0,0]],[[1,2],[0,2]],[[1,3],[0,4]],
                    [[2,1],[2,0]],[[2,2],[2,2]],[[2,3],[2,4]],
                    [[3,1],[4,0]],[[3,2],[4,2]],[[3,3],[4,4]]]
 
-print('\n')
-print("Player 1 is 'X' and Player 2 is 'O'\n")
-for i in board:
-    print(*i)
-print('\n')
+removed_positions = []
 
-player_1_input = []
-player_1_row = int(input("Player 1, Please enter a row number (1, 2, or 3): "))
-player_1_input.append(player_1_row)
-player_1_column = int(input("Player 1, Please enter a column number (1, 2, or 3): "))
-player_1_input.append(player_1_column)
-
-for i in board_positions:
-    if player_1_input == i[0]:
-        inputted = i[1]
-        board[inputted[0]].insert(inputted[1],"X")
-        board_positions.remove(i)
-        for i in board:
-            print(*i)
+def print_board():
+    print('\n')
+    print("Player 1 is 'X' and Player 2 is 'O'\n")
+    for i in board:
+        print(*i)
+    print('\n')
 
 
-# player_2_row = input("Player 2, Please enter a row number (1, 2, or 3: ")
-# player_2_column = input("Player 2, Please enter a column number (1, 2, or 3: ")
+def player_1():
+    global board
+    global board_positions
+    player_1_input = []
+    player_1_row = int(input("Player 1, Please enter a row number (1, 2, or 3): "))
+    player_1_input.append(player_1_row)
+    player_1_column = int(input("Player 1, Please enter a column number (1, 2, or 3): "))
+    player_1_input.append(player_1_column)
 
+    for i in board_positions:
+        if player_1_input == i[0]:
+            inputted = i[1]
+            board[inputted[0]].insert(inputted[1], "X")
+            board[inputted[0]].pop(inputted[1] + 1)
+            board_positions.remove(i)
+
+
+def player_2():
+    global board
+    global board_positions
+    player_2_input = []
+    player_2_row = int(input("Player 2, Please enter a row number (1, 2, or 3): "))
+    player_2_input.append(player_2_row)
+    player_2_column = int(input("Player 2, Please enter a column number (1, 2, or 3): "))
+    player_2_input.append(player_2_column)
+
+    for i in board_positions:
+        if player_2_input == i[0]:
+            inputted = i[1]
+            board[inputted[0]].insert(inputted[1], "O")
+            board[inputted[0]].pop(inputted[1] + 1)
+            board_positions.remove(i)
+
+
+game_on = True
+while game_on:
+    print_board()
+    player_1()
+    print_board()
+    player_2()
